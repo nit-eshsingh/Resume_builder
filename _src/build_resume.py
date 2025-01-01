bffd2d30159888e275aebed274d9ce9c3b177596
Nitesh <niteshksmgs@gmail.com>
2025-01-02 02:40:37 +0530

    first beta release

--- _src/build_resume.py
+++ _src/build_resume.py
@@ -98,10 +98,11 @@ def create_stylized_resume_pdf(file_name):
         print(f"{x_position},{y_position},add_title {text}")
         return draw_line(y_position - 5)
 
-    def draw_paragraph_text(pdf, text, x_position, y_position, usable_width):
+    def write_justified_text(pdf, text, x_position, y_position, usable_width):
         # Create a Paragraph with justified alignment
         styles = getSampleStyleSheet()
-        style = styles['BodyText']
+        style = styles['Bullet']
+        # style = styles['BodyText']
         style.alignment = 4  # 4 corresponds to justified alignment
 
         # Create a Paragraph
@@ -113,6 +114,44 @@ def create_stylized_resume_pdf(file_name):
         print(f"before y_position {y_position}")
         y_position = y_position - height
         print(f"{x_position},{y_position} ,draw_aligned_text {text}")
+        return y_position# Draw the text on the canvas
+
+    def write_justified_centered_text(pdf, text, x_position, y_position, usable_width, styling=None):
+        # Create a Paragraph with justified alignment
+        styles = getSampleStyleSheet()
+        if styling == "Heading1":
+            style = styles['Heading1']
+            style.alignment = 1  # 4 corresponds to justified alignment
+        if styling == "Normal":
+            # styles = getSampleStyleSheet()
+            style = styles['Normal']
+            style.alignment = 1  # 4 corresponds to justified alignment
+        pdf.setFillColor(colors.rgb2cmyk(0, 0, 9))
+        paragraph = Paragraph(text, style)
+        # Create a "fake" frame to wrap and justify text
+        _, height = paragraph.wrap(usable_width, usable_height)  # Calculate the height needed for the text
+        paragraph.drawOn(pdf, x_position, int(y_position))
+        pdf.setFillColor(colors.black)
+        print(f"before y_position {y_position}")
+        y_position = y_position - height
+        print(f"{x_position},{y_position} ,write_justified_centered_text {text}")
+        return y_position# Draw the text on the canvas
+
+    def write_justified_right_text(pdf, text, x_position, y_position, usable_width):
+        # Create a Paragraph with justified alignment
+        styles = getSampleStyleSheet()
+        style = styles['BodyText']
+        style.alignment = 2  # 2 corresponds to right alignment
+
+        # Create a Paragraph
+        paragraph = Paragraph(text, style)
+
+        # Create a "fake" frame to wrap and justify text
+        _, height = paragraph.wrap(usable_width, 0)  # Calculate the height needed for the text
+        paragraph.drawOn(pdf, x_position, int(y_position))
+        print(f"before y_position {y_position}")
+        # y_position = y_position - height
+        print(f"{x_position},{y_position} ,write_justified_right_text {text}")
         return y_position# Draw the text on the canvas
 
     # Function to add regular text
@@ -131,21 +170,23 @@ def create_stylized_resume_pdf(file_name):
             print(f"{x_position},{y_position} ,draw_text {text}")
         return y_position
 
-    # Header
-    pdf.setFont("Helvetica-Bold", 20)
-    pdf.setFillColor(colors.rgb2cmyk(0,0,1))
-    # pdf.drawString(30,y_position, "NITESH KUMAR SINGH")
-    pdf.drawCentredString(
-        page_width/2,
-        y_position,
-        f"{RESUME_NAME}",
-        mode=0)
-    y_position = draw_text(
-        f"{ADDRESS}  |  {PHONE}  |  {EMAILID}  |  {LINKEDIN}",
-        y_position - 20, indent=10
-    )
+    y_position = write_justified_centered_text(pdf, f"<b>{RESUME_NAME}</b>",x_position, y_position, usable_width, styling="Heading1")
+    personal_detail = f"<b>{ADDRESS} | {PHONE} | <u>{EMAILID}</u> | <u>{LINKEDIN}</u></b>"
+
+    write_justified_centered_text(pdf, personal_detail, x_position, y_position, usable_width, styling="Normal")
+    y_position = y_position - 12
+    # pdf.drawCentredString(
+    #     page_width/2,
+    #     y_position,
+    #     f"{RESUME_NAME}",
+    #     mode=0)
+
+    # y_position = draw_text(
+    #     f"{ADDRESS}  |  {PHONE}  |  {EMAILID}  |  {LINKEDIN}",
+    #     y_position - 20, indent=10
+    # )
     # personal_detail = f"<b>{ADDRESS} | {PHONE} | <u>{EMAILID}</u> | <u>{LINKEDIN}</u></b>"
-    # y_position = draw_paragraph_text(pdf, f"{personal_detail}", x_position, y_position, usable_width)
+    # y_position = write_justified_text(pdf, f"{personal_detail}", x_position, y_position, usable_width)
     # # print(y_position, "return loop")
     # y_position = y_position - 14
     # y_position = draw_line(y_position - 10)
@@ -164,24 +205,25 @@ def create_stylized_resume_pdf(file_name):
     # Professional Experience
     y_position = add_title("Experience:", y_position)
     print(y_position, "Experience")
+    bold_characters = {"ETL": "<b>ETL</b>", "Kafka": "<b>Kafka</b>"}
     experiences = [
         {
             "role": "Technical Specialist",
             "company": "CONTINENTAL AG",
             "duration": "July 2022 - Present",
             "details": [
-                "\u2022 Implemented an <b>ETL</b> process to transform <b>Kafka</b> streams into MongoDB, enabling continuous data processing with reduced latency.",
-                "\u2022 Developed a multi-branch pipeline using Jenkinsfile, streamlining the deployment process "
-                "and ensuring efficient code integration across different projects.",
-                "\u2022 Successfully deployed our local server on AWS using a Linux-based EC2 server and ODBC connector installation, optimizing infrastructure and enabling seamless scalability.",
-                "\u2022 Utilized Plant UML, database schemas, sequence diagrams, dynamic logger, and Confluence to ensure continuity in operations and effective collaboration with team members.",
-                "\u2022 Implemented static tool analyzers, including SonarQube, to improve code quality and maintain high standards of software development.",
-                "\u2022 Generated graphical representations of builds into plots on AWS server for visualization and analysis of code quality, facilitating data-driven decision making and continuous improvement.",
-                "\u2022 Developed web scraping techniques to extract data from Jira servers for ADAS projects, solved B+ tree of parent-child model using memoization.",
-                "\u2022 Built data extraction from GraphQL web service API response using multiprocessing for synchronization with the database, enhancing data integrity and system efficiency.",
-                "\u2022 Created a Microsoft flow using Power Automate diagram to obtain continuous integration of gateway failure into a Power BI report, enabling real-time monitoring.",
-                "\u2022 Collaborated with cross-functional teams to ensure seamless integration of ETL processes and web scraping techniques from different servers.",
-                "\u2022 Developed a OSS report generator project to scan all the python packages used inside the project and create a report for scanning for license clearance. Ex- MIT, Apache, BSD and GPL.\n"
+                f"\u2022 Implemented an {bold_characters.get('ETL')} process to transform {bold_characters.get('Kafka')} streams into MongoDB, enabling continuous data processing with reduced latency.",
+                f"\u2022 Developed a multi-branch pipeline using <b>Jenkinsfile</b>, streamlining the deployment process "
+                f"and ensuring efficient code integration across different projects.",
+                f"\u2022 Successfully deployed our local server on <b>AWS</b> using a Linux-based EC2 server and ODBC connector installation, optimizing infrastructure and enabling seamless scalability.",
+                f"\u2022 Utilized Plant <b>UML</b>, database schemas, sequence diagrams, dynamic logger, and Confluence to ensure continuity in operations and effective collaboration with team members.",
+                f"\u2022 Implemented static tool analyzers, including <b>SonarQube</b>, to improve code quality and maintain high standards of software development.",
+                f"\u2022 Generated <b>graphical</b> representations of builds into plots on AWS server for visualization and analysis of code quality, facilitating data-driven decision making and continuous improvement.",
+                f"\u2022 Developed web scraping techniques to extract data from Jira servers for ADAS projects, solved B+ tree of parent-child model using <b>memoization</b>.",
+                f"\u2022 Built data extraction from <b>GraphQL</b> web service API response using <b>multiprocessing</b> for synchronization with the database, enhancing data integrity and system efficiency.",
+                f"\u2022 Created a Microsoft flow using <b>Power Automate</b> diagram to obtain continuous integration of gateway failure into a Power BI report, enabling real-time monitoring.",
+                f"\u2022 Collaborated with cross-functional teams to ensure seamless integration of ETL processes and <b>web scraping</b> techniques from different servers.",
+                f"\u2022 Developed a <b>OSS</b> report generator project to scan all the python packages used inside the project and create a report for scanning for license clearance. Ex- MIT, Apache, BSD and GPL.\n"
             ]
         },
         {
@@ -189,15 +231,16 @@ def create_stylized_resume_pdf(file_name):
             "company": "Bosch Global Software Technologies",
             "duration": "Aug 2018 - July 2022",
             "details": [
-                "\u2022 Spearheaded a development team in the creation of a software application for configuring communication in heavy automotive, leveraging the power and versatility of Python and the PyQt5.",
+                "\u2022 <b>Spearheaded</b> a development team in the creation of a software application for configuring "
+                "communication in heavy automotive, leveraging the power and versatility of Python and the <b>PyQt5.</b>",
                 "\u2022 Developed Python tools, both command-line interface based and desktop applications, for mobile hydraulics, enhancing efficiency and productivity.",
-                "\u2022 Created a project Elf2A2L to parse non-human readable files Executable and Linkable Format (.elf) for data analytics and conversion into a readable format ASAM MCD-2 MC Language.(.a2l), improving data accessibility and usability.",
-                "\u2022 Served as Scrum Master and Engineering Product Quality (EPQ) auditor for a Web framework development project, ensuring adherence of CMMI level-5 standard.",
-                "\u2022 Enhanced the user experience by simplifying the process of automatic code generation, for heavy mobile hydraulic vehicle for different OEMs.",
+                "\u2022 Created a project <b>Elf2A2L</b> to parse non-human readable files Executable and Linkable Format (.elf) for data analytics and conversion into a readable format ASAM MCD-2 MC Language.(.a2l), improving data accessibility and usability.",
+                "\u2022 Served as <b>Scrum Master</b> and Engineering Product Quality (<b>EPQ</b>) auditor for a Web framework development project, ensuring adherence of CMMI level-5 standard.",
+                "\u2022 Enhanced the user experience by simplifying the process of automatic code generation, for heavy mobile hydraulic vehicle for different <b>OEMs</b>.",
                 "\u2022 Created a robust back-end data handling structure using established software design patterns, enhancing the reliability and scalability of the software development.",
-                "\u2022 Optimized software performance by reducing execution time by up to 50%, improving overall system efficiency and responsiveness.",
-                "\u2022 Implemented Jenkins pipeline stages, including repository checkout, static code analyzer, test run on source code, FOSS ID run (OSS report generation), executable creation, test run on executable and delivery.",
-                "\u2022 Designed a user-friendly HMI panel for control-x hardware used for Drives and controls using UI/UX Touchgfx software from stm32.",
+                "\u2022 Optimized software performance by reducing execution time by up to <b>50%</b>, improving overall system efficiency and responsiveness.",
+                "\u2022 Implemented Jenkins pipeline stages, including repository checkout, static code analyzer, test run on source code, <b>FOSS ID</b> run (OSS report generation), executable creation, test run on executable and delivery.",
+                "\u2022 Designed a user-friendly <b>HMI panel</b> for control-x hardware used for Drives and controls using UI/UX Touchgfx software from stm32.",
             ]
         }
     ]
@@ -205,22 +248,43 @@ def create_stylized_resume_pdf(file_name):
     for exp in experiences:
         y_position = y_position - 8
         print(y_position, "experiences in loop")
-        y_position = draw_text(f"{exp['role']} | {exp['company']} ({exp['duration']})", y_position, bold=True)
+        # write_justified_right_text()
+        write_justified_right_text(pdf, f"<b>({exp['duration']})</b>", x_position, y_position, usable_width+10)
+        y_position = draw_text(f"{exp['role']} | {exp['company']}", y_position, bold=True)
         for detail in exp['details']:
             # print(y_position, "loop")
-            y_position = draw_paragraph_text(pdf,f"{detail}",x_position, y_position, usable_width)
+            y_position = write_justified_text(pdf,f"{detail}",x_position, y_position, usable_width)
             # print(y_position, "return loop")
         y_position = y_position - 14
 
     # Education
     y_position = add_title("Education:", y_position)
-    education_detail = ["<b>B.Tech</b> : Electronics and Communication Engineering,<b>NIT Hamirpur</b> (2014 - 2018) | 8.21 CGPA ",
-              "<b>Senior Secondary</b> : Laxmi Public School (2011 - 2013) | 93.6% </p>"]
+    education_detail = [
+        {
+            "college": "<b>National Institute of Technology, Hamirpur (NITH)</b>",
+            "course": "<i>Bachelor of Technology in Electronics and Communication Engineering</i>",
+            "duration": "<b>Aug 2014 - May 2018</b>",
+            "grades": "<i>CGPA : 8.21</i>",
+            "location": "<i>Hamirpur, H.P., India</i>"
+        },
+        {
+            "college": "<b>Laxmi Public School (LPS)</b>",
+            "course": "<i>Senior Secondary </i>",
+            "duration": "<b>Apr 2011 - May 2013</b>",
+            "grades": "<i>Percentage : 93.6%</i>",
+            "location": "<i>DDU, U.P., India</i>"
+        }
+    ]
+
+    # education_detail = ["<b>National Institute of Technology</b>",
+    #                     "<i>Bachelor of Technology in Electronics and Communication Engineering</i> | 8.21 CGPA ",
+    #                     "<b>Laxmi Public School</b>",
+    #                     "<i>Senior Secondary </i> | <b>93.6%</b>"]
     for detail_item in education_detail:
-        # print(y_position, "loop")
-        y_position = draw_paragraph_text(pdf, f"{detail_item}", x_position, y_position, usable_width)
-        # print(y_position, "return loop")
-    # y_position = y_position - 14
+        y_position = write_justified_text(pdf, f"{detail_item['college']}", x_position, y_position, usable_width)
+        write_justified_right_text(pdf, f"({detail_item['duration']})", x_position, y_position, usable_width + 10)
+        y_position = write_justified_text(pdf, f"{detail_item['course']} | {detail_item['grades']}", x_position, y_position, usable_width)
+        write_justified_right_text(pdf, f"{detail_item['location']}", x_position, y_position, usable_width + 10)
     y_position = y_position - 10
 
     # Skills
@@ -232,36 +296,27 @@ def create_stylized_resume_pdf(file_name):
     #     "Others: MongoDB, GraphQL, Kafka, Artifactory Servers, SonarQube, Power Automate",
     #     y_position
     # )
-    skill_detail = ["<b>Python:</b>PySpark, PyQt5, Web Scraping, pandas, numpy, psycopg2.\n",
-        "<b>DevOps:</b> AWS, Jenkins, Groovy, Batch Script, FOSS ID.\n",
-        "<b>Design Patterns:</b> UML Diagrams, DB Schemas, Sequence Diagram, ETL.\n",
-        "<b>Others:</b> MongoDB, PostgreSQL, GraphQL, Kafka, Artifactory Servers, SonarQube, Power Automate."]
+    skill_detail = ["<b>Python:</b>PySpark, fastapi, PyQt5, Web Scraping, pandas, numpy, psycopg2.\n",
+        "<b>DevOps :</b> AWS, Jenkins, Groovy, Batch Script, FOSS ID.\n",
+        "<b>Design Patterns :</b> UML Diagrams, DB Schemas, Sequence Diagram, ETL.\n",
+        "<b>Others :</b> MongoDB, PostgreSQL, GraphQL, Kafka, Artifactory Servers, SonarQube, Power Automate."]
 
     for detail_item in skill_detail:
-        # print(y_position, "loop")
-        y_position = draw_paragraph_text(pdf, f"{detail_item}", x_position, y_position, usable_width)
-        # print(y_position, "return loop")
+        y_position = write_justified_text(pdf, f"{detail_item}", x_position, y_position, usable_width)
     y_position = y_position - 10
-    # y_position = draw_paragraph_text(pdf, f"{detail}", 30, y_position, usable_width)
-    # print(y_position, "return loop")
-    # y_position = y_position - 12
 
     # Publications
     y_position = add_title("Publications:", y_position)
-    publication_detail = ["<b>Co-Author:</b> Energy detection based spectrum sensing for gamma shadowed α–η–μ and α–κ–μ fading channels",
+    hyperlink = '<a href="https://www.sciencedirect.com/science/article/abs/pii/S1434841118301791"><u>Energy detection based spectrum sensing for gamma shadowed α–η–μ and α–κ–μ fading channels.</u></a>'
+    # hyperlink = '<a href="https://www.sciencedirect.com/science/article/abs/pii/S1434841118301791"><b>Co-Author:</b> Energy detection based spectrum sensing for gamma shadowed α–η–μ and α–κ–μ fading channels</a>'
+    publication_detail = [f"<b>Co-Author: {hyperlink}</b>",
         "<b>Journal:</b> International Journal of Electronics and Communications",
-        "<b>Published:</b> September 1, 2018",]
+        "<b>Published:</b> September 1, 2018",
+        "Collaborated with Dr. Sandeep Kumar (Principal Scientist), Dr. Kuldeep Singh and the research team to design and implement innovative solutions enhancing radar navigation systems through advanced spectrum sensing techniques."]
     for detail_item in publication_detail:
-        # print(y_position, "loop")
-        y_position = draw_paragraph_text(pdf, f"{detail_item}", x_position, y_position, usable_width)
-        # print(y_position, "return loop")
+        y_position = write_justified_text(pdf, f"{detail_item}", x_position, y_position, usable_width)
+
     y_position = y_position - 10
-    # y_position = add_title("Publications:", y_position)
-    # y_position = draw_text(
-    #     'Co-Author: "Energy detection based spectrum sensing for gamma shadowed α–η–μ and α–κ–μ fading channels"\n'
-    #     "Journal: International Journal of Electronics and Communications\nPublished: September 1, 2018",
-    #     y_position
-    # )
 
     # Languages
     # y_position = add_title("Languages:", y_position)
@@ -269,13 +324,8 @@ def create_stylized_resume_pdf(file_name):
 
     # Hobbies & Interests
     y_position = add_title("Hobbies & Interests:", y_position)
-    # y_position = y_position - 8
-    # y_position = draw_text(
-    #     ,
-    #     y_position
-    # )
     hobbies_item = "Traveling and exploring places, Listening to music, Chess, Badminton, and Table Tennis"
-    draw_paragraph_text(pdf, f"{hobbies_item}", x_position, y_position, usable_width)
+    write_justified_text(pdf, f"{hobbies_item}", x_position, y_position, usable_width)
 
     # Save the PDF
     pdf.save()
